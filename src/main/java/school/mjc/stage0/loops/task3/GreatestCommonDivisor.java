26ab619697a506430d1b3ddad26c6d1a1618cf85
Zotoff <paulzotoff@gmail.com>
2023-06-16 23:06:14 +0300

    First attempt

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        while (second > 0)  {
+            int r = second;
+            second = first % second;
+            first = r;
+        }
+        System.out.println(first);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        int sum = 0;
+        if (firstBoarder == secondBoarder) {
+            sum = firstBoarder;
+        } else if (firstBoarder % secondBoarder == 0) {
+            sum = 0;
+        }
+        else
+        {
+            for (int i = firstBoarder; i < secondBoarder; i++) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
     }
 }
